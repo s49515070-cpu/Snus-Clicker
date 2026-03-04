c2607ed5b1a92d7e6b4a6980775ec42276e07b6f
s49515070-cpu <s49515070@gmail.com>
2026-03-04 13:32:57 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,10 @@
-import { gameLoop } from "./engine.js";
-import { renderUI, renderBuildings, renderPrestigeUpgrades, applyWorldTheme } from "./ui.js";
-import { loadGame, saveGame } from "./save.js";
+import { updateGame } from "./engine.js";
+import { render } from "./ui.js";
 
-function init() {
-    loadGame();
-    applyWorldTheme();
-    renderBuildings();
-    renderPrestigeUpgrades();
-
-    gameLoop();
-    uiLoop();
-
-    setInterval(saveGame, 5000);
+function gameLoop() {
+    updateGame();
+    render();
 }
 
-function uiLoop() {
-    renderUI();
-    renderBuildings();
-    requestAnimationFrame(uiLoop);
-}
-
-init();
+setInterval(gameLoop, 100);
+render();
